49f8f5e7660b4440fa148f26b0371429e2dfa251
spacegirlkes <33458645+spacegirlkes@users.noreply.github.com>
2021-09-24 21:35:52 -0500

    added 2 files from the class

---
+++ CS50_Python_JS_web/GitHub Examples/loops.py
@@ -0,0 +1,10 @@
+for i in [0, 1, 2, 3, 4, 5]: # square brackets indicate a list
+    print(i)
+
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Ron", "Hermione"]
+
+for name in names:
+    print(name)
