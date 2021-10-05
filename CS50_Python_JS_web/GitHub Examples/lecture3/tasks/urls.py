7acd49ce5ac90d800e1676abfd618c6aa6e67214
spacegirlkes <33458645+spacegirlkes@users.noreply.github.com>
2021-10-04 21:51:07 -0500

    More project files

--- CS50_Python_JS_web/GitHub Examples/lecture3/tasks/urls.py
+++ CS50_Python_JS_web/GitHub Examples/lecture3/tasks/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks" # unique identifier to make sure you are calling the right app to render. See index.html line 10
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add", views.add, name="add") # calling the "add" def from views.py
 
 ]
--- CS50_Python_JS_web/GitHub Examples/lecture3/tasks/views.py
+++ CS50_Python_JS_web/GitHub Examples/lecture3/tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks # django gives us access to tasks on the right
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")
